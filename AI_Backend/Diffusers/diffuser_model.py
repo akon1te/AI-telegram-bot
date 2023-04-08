6cb9b927ae849d1c4305d9e823aa3a6e1d5f551f
akon1te <anros_t@mail.ru>
2023-04-09 00:27:05 +0300

    add text generation way

--- AI_Backend/Diffusers/diffuser_model.py
+++ AI_Backend/Diffusers/diffuser_model.py
@@ -1,15 +1,51 @@
-from diffusers import StableDiffusionImageVariationPipeline
+from diffusers import StableDiffusionImageVariationPipeline, StableDiffusionPipeline
+from transformers import WhisperProcessor, WhisperForConditionalGeneration
 from PIL import Image
+import torch
 from torchvision import transforms
 
-def generate_picture(pic_path: str, save_path: str, user_id) -> str:
-    
-    device = "cuda:0"
-    sd_pipe = StableDiffusionImageVariationPipeline.from_pretrained(
+"""
+def sound_preproccessing():
+
+    # load model and processor
+    processor = WhisperProcessor.from_pretrained("openai/whisper-base")
+    model = WhisperForConditionalGeneration.from_pretrained(
+        "openai/whisper-base")
+    forced_decoder_ids = processor.get_decoder_prompt_ids(
+        language="russian", task="translate")
+
+    # load streaming dataset and read first audio sample
+    input_speech = 0
+    input_features = processor(
+        input_speech["array"], sampling_rate=input_speech["sampling_rate"], return_tensors="pt").input_features
+
+    # generate token ids
+    predicted_ids = model.generate(
+        input_features, forced_decoder_ids=forced_decoder_ids)
+    # decode token ids to text
+    transcription = processor.batch_decode(
+        predicted_ids, skip_special_tokens=True)
+"""
+
+def sd_create_from_text(text: str, save_path: str, user_id):
+
+    sd_pipeline = StableDiffusionPipeline.from_pretrained(
+        "runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16)
+    sd_pipeline = sd_pipeline.to("cuda")
+
+    out = sd_pipeline(text)
+    out['images'][0].save(f"{save_path}/{user_id}_result.jpg")
+    return f"{save_path}/{user_id}_result.jpg"
+
+
+def sd_create_from_picture(pic_path: str, save_path: str, user_id) -> str:
+
+    sd_pipeline = StableDiffusionImageVariationPipeline.from_pretrained(
         "lambdalabs/sd-image-variations-diffusers",
         revision="v2.0",
     )
-    sd_pipe = sd_pipe.to(device)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    sd_pipeline = sd_pipeline.to(device)
 
     im = Image.open(pic_path)
     tform = transforms.Compose([
@@ -26,6 +62,6 @@ def generate_picture(pic_path: str, save_path: str, user_id) -> str:
     ])
     inp = tform(im).to(device).unsqueeze(0)
 
-    out = sd_pipe(inp, guidance_scale=3)
+    out = sd_pipeline(inp, guidance_scale=3)
     out['images'][0].save(f"{save_path}/{user_id}_result.jpg")
     return f"{save_path}/{user_id}_result.jpg"
--- utils/texts.py
+++ utils/texts.py
@@ -5,6 +5,10 @@ no_pics = (
     f"You didnt load any pictures!"
 )
 
+create_paths = (
+    f"Выберите как хотите создать фотку.\n Можно фото -> фото, текст -> фото, аудио -> фото"
+)
+
 help_text = (
     f"Write me something and I will try to answer your question or complete your task"
 )
